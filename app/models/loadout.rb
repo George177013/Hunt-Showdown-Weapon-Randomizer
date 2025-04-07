2f4163dbbacdb2c9aff0c402eca12ee858f5666a
george <george172@outlook.com>
2025-04-07 23:48:14 +0200

    randomizer done so far and working

--- app/models/loadout.rb
+++ app/models/loadout.rb
@@ -1,9 +1,6 @@
 class Loadout
   attr_reader :primary_weapon, :secondary_weapon, :ammos, :consumables, :tools
 
-  # TODO: No duallies
-  # TODO: Froce medkit and melee
-
   def initialize
     @primary_weapon = { name: "", price: 0 }
     @secondary_weapon = { name: "", price: 0 }
@@ -13,15 +10,18 @@ class Loadout
     @total_cost = 0
   end
 
-  def randomize
-    config = {
-      slots: 5,
-      budget: 10000,
-      bloodline_rank: 100,
-      tools_count: 4,
-      consumables_count: 4,
-      intelligent: true
-    }
+  def randomize(config)
+    # config = {
+    #   slots: 4,
+    #   budget: 10000,
+    #   bloodline_rank: 100,
+    #   tools_count: 4,
+    #   consumables_count: 4,
+    #   intelligent: true,
+    #   dualies: true,
+    #   force_medkit: true,
+    #   force_melee: true
+    # }
 
     max_attempts = 32
     attempts = 0
@@ -36,26 +36,27 @@ class Loadout
 
       attempts += 1
 
+      weapon_scope = Weapon.where("bloodline_rank <= ?", config[:bloodline_rank])
+      weapon_scope = weapon_scope.where.not(kind: "pistol_dual") unless config[:dualies]
+
       if config[:intelligent]
         kind_categories = {
           long: [ "rifle" ],
           mid: [ "pistol" ],
-          short: [ "shotgun", "pistol_dual", "special" ]
+          short: [ "shotgun", "special", "pistol_dual", "melee" ]
         }
 
-        @primary_weapon = Weapon.where("bloodline_rank <= ?", config[:bloodline_rank]).where("size >= 2").order("RANDOM()").first
-
+        @primary_weapon = weapon_scope.where("size >= 2").order("RANDOM()").first
         slots_left = config[:slots] - @primary_weapon.size
 
         pw_category = kind_categories.find { |k, arr| arr.include?(@primary_weapon.kind) }&.first
-
         kinds_left = kind_categories.reject { |key, _| key == pw_category }.values.flatten
 
-        @secondary_weapon = Weapon.where("bloodline_rank <= ?", config[:bloodline_rank]).where(kind: kinds_left).where(size: slots_left).order("RANDOM()").first
+        @secondary_weapon = weapon_scope.where(kind: kinds_left, size: slots_left).order("RANDOM()").first
       else
-        @primary_weapon = Weapon.where("bloodline_rank <= ?", config[:bloodline_rank]).where("size >= 2").order("RANDOM()").first
+        @primary_weapon = weapon_scope.where("size >= 2").order("RANDOM()").first
         slots_left = config[:slots] - @primary_weapon.size
-        @secondary_weapon = Weapon.where("bloodline_rank <= ?", config[:bloodline_rank]).where(size: slots_left).order("RANDOM()").first
+        @secondary_weapon = weapon_scope.where(size: slots_left).order("RANDOM()").first
       end
 
       if @primary_weapon.ammos.where(pool: 2).exists?
@@ -77,6 +78,14 @@ class Loadout
       end
 
       @tools = Tool.where("bloodline_rank <= ?", config[:bloodline_rank]).order("RANDOM()").first(config[:tools_count])
+
+      if config[:force_medkit] && !@tools.any? { |tool| tool.name == "First Aid Kit" }
+        @tools[0] = Tool.where(name: "First Aid Kit").first
+      end
+
+      if config[:force_melee] && !@tools.any? { |tool| tool.kind == "melee" } && !@secondary_weapon.kind == "melee"
+        @tools[1] = Tool.where("bloodline_rank <= ?", config[:bloodline_rank]).where(kind: "melee").order("RANDOM()").first
+      end
 
       config[:consumables_count].times do
         @consumables << Consumable.where("bloodline_rank <= ?", config[:bloodline_rank]).order("RANDOM()").first
